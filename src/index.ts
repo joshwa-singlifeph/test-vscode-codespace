f4d7251e8490f290e70cf4009fed5efbb512bc20
Joshwa Facistol <111828779+joshwa-singlifeph@users.noreply.github.com>
2023-11-23 03:38:31 +0000

    add lint

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-import express, { Express, Request, Response } from 'express';
+import express, { Express, Request, Response } from 'express'
 
-const app: Express = express();
+const app: Express = express()
 
 app.get('/', (req: Request, res: Response) => {
-    return res.json({ success: true });
-});
+  return res.json({ success: true })
+})
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000
 
-app.listen(port, () => console.log(`Server running on port: ${port}`));
+app.listen(port, () => console.log(`Server running on port: ${port}`))
